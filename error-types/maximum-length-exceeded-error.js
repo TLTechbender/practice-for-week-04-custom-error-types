28a40c71faf723c6e941ea80c513e730320da8f3
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-17 16:57:04 +0100

    feat: Passed tests

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,7 +1,19 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError{
+//I got a bit confused here so I needed help.....
+	constructor(length=null, ...params){
+	super(...params);
+    this.name= "MaximumLengthExceededError";
+	this.message = "Maximum length exceeded";
 
+   if(length){
+   this.message = this.message + ` by ${length}`;
+
+   }
+};
+};
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -2,7 +2,20 @@
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+class ValidationError extends Error{
+constructor(message=null, ...params){
+super(...params);
 
+if(Error.captureStackTrace){
+	Error.captureStackTrace(this,ValidationError);
+}
+
+    this.name = "ValidationError";
+    this.message = /*this.message*/ message || "Invalid input";
+}
+
+
+};
 try {
   module.exports = ValidationError;
 } catch {
